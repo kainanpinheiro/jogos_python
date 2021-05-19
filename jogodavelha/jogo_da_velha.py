a306cf562e147b418b27d8aba3e612974ff843de
kainanpinheiro <kainan.psilva@gmail.com>
2021-05-19 20:52:51 -0300

    Criada função que valida se houve ganhador

--- jogodavelha/jogo_da_velha.py
+++ jogodavelha/jogo_da_velha.py
@@ -1,24 +1,26 @@
 class JogoDaVelha:
     def __init__(self, jogador1, jogador2):
-        self.jogador1 = jogador1
-        self.jogador2 = jogador2
-        self.ganhador = False
+        self.__jogador1 = jogador1
+        self.__jogador2 = jogador2
+        self.__ganhador = False
         self.lugares = [
             [1, 2, 3],
             [4, 5, 6],
             [7, 8, 9]
         ]
 
+    def get_ganhador(self):
+        return self.__ganhador
+
     def iniciar(self, jogada, jogador):
         print(self.velha())
-        while not self.ganhador:
-            retorno = self.escolher_lugar(jogada, jogador)
-            if retorno:
-                print(self.velha())
-                return True
-            else:
-                print(self.velha())
-                return "Jogada inválida"
+        retorno = self.escolher_lugar(jogada, jogador)
+        if retorno:
+            print(self.velha())
+            retorno = self.verificar_ganhador()
+        else:
+            print(self.velha())
+            return "Jogada inválida"
 
     def velha(self):
         tabuleiro = f"""
@@ -38,6 +40,37 @@ class JogoDaVelha:
                     return True
         return False
 
+    def verificar_ganhador(self):
+        ganhou = list()
 
-jogo = JogoDaVelha("X", "O")
-jogo.iniciar(1, "X")
+        # Verifica valores na horizontal
+        for lugar in self.lugares:
+            ganhou.append(self.validar_valores(lugar))
+
+        # Verifica valores na vertical
+        for lugar in zip(*self.lugares):
+            ganhou.append(self.validar_valores(list(lugar)))
+
+        # Verifica valores na diagonal
+        diagonal1 = [lugar[i] for i, lugar in enumerate(self.lugares)]
+        diagonal2 = [lugar[i] for i, lugar in enumerate(self.lugares[::-1])]
+        ganhou.append(self.validar_valores(diagonal1))
+        ganhou.append(self.validar_valores(diagonal2))
+
+        if any(ganhou):
+            self.__ganhador = True
+
+    def validar_valores(self, lugar):
+        return all(i == lugar[0] for i in lugar)
+
+
+if __name__ == "__main__":
+    jogo = JogoDaVelha("X", "O")
+    ganhador = jogo.get_ganhador()
+    jogo.ganhador = True
+    while not ganhador:
+        jogo.iniciar(1, "O")
+        jogo.iniciar(4, "O")
+        jogo.iniciar(7, "O")
+        ganhador = jogo.get_ganhador()
+    print('FINALIZADO')
